126f830932d2185b90e7a80b54d9b108df7c73b8
magnus kofoed <magnus@kofoed2010.dk>
2022-12-20 22:49:22 +0100

    spawns

--- Assets/Scripts/GameNetworkManager.cs
+++ Assets/Scripts/GameNetworkManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using Mirror;
 
 public class GameNetworkManager : NetworkManager
-{
+{ 
+    
     public struct CreatePlayer : NetworkMessage
     {
         public string name;
         public int score;
+        public Vector3 spawn;
+       
     }
     public override void OnStartServer()
     {
@@ -25,10 +28,13 @@ public class GameNetworkManager : NetworkManager
         switch (NetworkServer.connections.Count)
         {
             case 0:
+                
                 CreatePlayer playermsg3 = new CreatePlayer
                 {
+                    
                     name = "Joe dad",
                     score = 0,
+                    spawn = new Vector3(-11,0,-6),
                 };
                 NetworkClient.Send(playermsg3);
             break;
@@ -38,6 +44,7 @@ public class GameNetworkManager : NetworkManager
                 {
                     name = "Joe Mama",
                     score = 0,
+                    spawn = new Vector3(-9,0,-6)
                 };
                 NetworkClient.Send(playermsg);
             break;
@@ -47,6 +54,7 @@ public class GameNetworkManager : NetworkManager
                 {
                     name = "karl",
                     score = 0,
+                    spawn = new Vector3(-9,0,-6)
                 };
                 NetworkClient.Send(playermsg1);
             break;
@@ -57,6 +65,7 @@ public class GameNetworkManager : NetworkManager
                 {
                     name = "julemand",
                     score = 0,
+                    spawn = new Vector3(-9,0,-6)
                 };
                 NetworkClient.Send(playermsg2);
             break;
@@ -68,7 +77,7 @@ public class GameNetworkManager : NetworkManager
 
     void OnCreatePlayer(NetworkConnectionToClient conn, CreatePlayer msg)
     {
-        GameObject playerObj = Instantiate(playerPrefab);
+        GameObject playerObj = Instantiate(playerPrefab, msg.spawn,Quaternion.identity);
         playerObj.GetComponent<PlayerMovement>().playername = msg.name;
         playerObj.GetComponent<PlayerMovement>().score = msg.score;
         StartCoroutine(RequestHandler.Instance.CreatePlayer(msg.name, msg.score));
